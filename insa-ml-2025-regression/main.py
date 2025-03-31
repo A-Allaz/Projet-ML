ad9b80be6d1389e4f071af6a860780bfd302508e
alexisgobin <alexis.gobin@insa-lyon.fr>
2025-03-31 11:34:45 +0200

    preprocess and main uploaded

--- insa-ml-2025-regression/main.py
+++ insa-ml-2025-regression/main.py
@@ -1,21 +1,98 @@
 # labraries
 import pandas as pd
-
+from sklearn import model_selection
 # this file is used to run models
 # it will import models and try to run them on the data
 from preprocess_data import preprocess_data_target
+from models import LinearRegressionModel, MultiLayerFeedForwardModel
+
+def get_datasets(data_path: str, submission_data_path: str, column_target: str, normalisation: bool = True):
+    """
+    Arrange the dataset by dropping the target column and returning the features and target separately.
+    
+    :param dataset: DataFrame containing the dataset
+    :param column_target: The name of the target column to be dropped
+    :return: Tuple of features (X) and target (Y)
+    """
+    dataset, X_submission, min_co2, max_co2 = preprocess_data_target(
+        data_path=data_path,
+        submission_data_path=submission_data_path,
+        normalisation=normalisation,
+        save_path='data/1.csv')
+
+    X = dataset.drop(columns=[column_target])
+    Y = dataset[column_target]
+
+
+    return X, Y, X_submission, min_co2, max_co2
+
+def save_submission(X_sub, Y_sub, save_path: str):
+    """
+    Save the submission data to a CSV file.
+    
+    :param X_sub: Feature matrix for submission
+    :param Y_sub: Predicted values for submission
+    :param datapath: Path to save the submission file
+    """
+    # return format
+    # id,co2
+    # 41257,527
+    # 41258,196
+    # 41259,457
+    # etc.
+
+    # Y_sub has to be rounded
+    Y_sub = Y_sub.round().astype(int)
+    submission = pd.DataFrame({
+        'id': X_sub['id'],
+        'co2': Y_sub
+    })
+    # save the submission
+    submission.to_csv(save_path, index=False)
+    print(f'Submission saved to {save_path}')
+
+
 
 if __name__ == '__main__':
     data_path: str = 'data/train.csv'
+    submission_data_path: str = 'data/submission_test.csv'
+    normalisation: bool = False
 
     # preprocess the data
-    dataset = preprocess_data_target(data_path, 'data/preprocess.csv')
+    X, Y, X_submission, min_co2, max_co2 = get_datasets(
+        data_path=data_path,
+        submission_data_path=submission_data_path,
+        column_target='co2',
+        normalisation=normalisation
+    )
+
+    # split the data into train and test sets
+    X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y, test_size=0.0001, random_state=42)
+
+
+    # # import the model
+    # model = LinearRegressionModel()
+    model = MultiLayerFeedForwardModel(input_size=X_train.drop(columns=['id']).shape[1], config_path='network_configs/1.json')
     
-    # import the model
+    # train the model (without id column)
+    model.fit(X_train.drop(columns=['id']), Y_train, epochs=10000)
+
+    # # test the model
+    # mae = model.test(X_test.drop(columns=['id']), Y_test)
+    # print(f'Mean Absolute Error: {mae}')
+
+    # # export for submission
+
+    # # predict on the submission data
+    # Y_submission_norm = model.predict(X_submission.drop(columns=['id']))
+    # # denormalize the predicted values
+    # Y_submission = Y_submission_norm
+    # if normalisation:
+    #     Y_submission = Y_submission_norm * (max_co2 - min_co2) + min_co2
 
 
-    # run the model
+    # # save the submission
+    # save_path = 'submission_data/linear_regression_unormalised.csv'
+    # save_submission(X_submission, Y_submission, save_path)
 
-
-    # try models on data/submission_test.csv (we don’t have the output of the submission_test.csv)
-    pass
+    # save the model
--- insa-ml-2025-regression/preprocess_data.py
+++ insa-ml-2025-regression/preprocess_data.py
@@ -2,41 +2,54 @@ import pandas as pd
 import numpy as np
 import os
 
-def preprocess_data_target(data_path, save_path):
+def preprocess_data_target(data_path, submission_data_path, normalisation, save_path:str = None):
     data = pd.read_csv(data_path)
+    submission_data = pd.read_csv(submission_data_path)
 
 
     # On récupère les colonnes dont les valeurs sont des chaînes de caractères
     string_columns = data.select_dtypes(include=['object']).columns
     
-    # On récupère les colonnes qui ont plus de 30 valeurs uniques
+    # On récupère les colonnes qui ont plus d’une valeur unique
     high_cardinality_columns = [col for col in string_columns if data[col].nunique() > 1]
-    print(high_cardinality_columns)
+
+
     # On Récupère la moyenne d’émission de CO2 pour chaque valeur unique des colonnes à haute cardinalité
     mean_co2_per_value = {}
     for col in high_cardinality_columns:
         mean_co2_per_value[col] = data.groupby(col)['co2'].mean()
-    print(mean_co2_per_value)
+
     # On remplace les valeurs des colonnes à haute cardinalité par la moyenne d’émission de CO2 correspondante
     for col in high_cardinality_columns:
         data[col] = data[col].map(mean_co2_per_value[col])
+        submission_data[col] = submission_data[col].map(mean_co2_per_value[col])
 
     # On remplace les valeurs manquantes par la moyenne de la colonne
     data = data.fillna(data.mean())
+    submission_data = submission_data.fillna(data.mean())
 
-    # On sauvegarde le fichier
-    data.to_csv(save_path, index=False)
 
-    # On normalise les colonnes en mettant à l’échelle les valeurs entre 0 et 1
-    for col in data.columns:
-        data[col] = (data[col] - data[col].min()) / (data[col].max() - data[col].min())
-        
     min_co2 = data['co2'].min()
     max_co2 = data['co2'].max()
 
-    return data, min_co2, max_co2
 
-if def __name__ == '__main__':
+
+    # On normalise les colonnes en mettant à l’échelle les valeurs entre 0 et 1
+    if normalisation:
+        for col in data.columns.drop(['id']):
+            if col != 'co2':
+                submission_data[col] = (submission_data[col] - data[col].min()) / (data[col].max() - data[col].min())
+            data[col] = (data[col] - data[col].min()) / (data[col].max() - data[col].min())
+            
+    
+    # On sauvegarde le fichier
+    if save_path:
+        data.to_csv(save_path, index=False)
+        submission_data.to_csv(save_path.replace('.csv', '_submission.csv'), index=False)
+
+    return data, submission_data, min_co2, max_co2
+
+if __name__ == '__main__':
     fileToClean = 'data/train.csv'
     cleanedFile = 'data/cleaned_train.csv'
     preprocess_data_target(fileToClean, cleanedFile)
