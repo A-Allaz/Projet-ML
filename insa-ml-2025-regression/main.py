32c8037d4ea23e7b1261a1f3a2405980c80ed26c
alexisgobin <alexis.gobin@insa-lyon.fr>
2025-03-31 09:14:52 +0200

    clean main

--- insa-ml-2025-regression/main.py
+++ insa-ml-2025-regression/main.py
@@ -1,18 +1,20 @@
+# labraries
+import pandas as pd
+
 # this file is used to run models
 # it will import models and try to run them on the data
+from preprocess_data import preprocess_data_target
 
 if __name__ == '__main__':
-    # get the data from the training set (data/train.csv)
+    data_path: str = 'data/train.csv'
 
-
-
+    # preprocess the data
+    dataset = preprocess_data_target(data_path, 'data/preprocess.csv')
+    
     # import the model
 
 
-
     # run the model
-
-
 
 
     # try models on data/submission_test.csv (we don’t have the output of the submission_test.csv)
